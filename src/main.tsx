fc2b6c22a7e1d38604516c4a565d86a4ef7509f3
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-11 08:44:10 +0100

    Load WP3 formation piece styling

--- src/main.tsx
+++ src/main.tsx
@@ -28,6 +28,7 @@ import './map-readability.css';
 import './r2-tactical-map.css';
 import './r3-strategic-map.css';
 import './r3-map-hierarchy.css';
+import './r3-formation-pieces.css';
 import './responsive-command-fit.css';
 import { installMapTrackpadGuard } from './map-trackpad-guard';
 
