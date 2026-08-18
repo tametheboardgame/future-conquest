8d3362ffcc1b958ce1e3a9b82892a1d0ccc014d3
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-18 07:17:35 +0100

    R3-WP6: load pictorial formation detail layer

--- src/main.tsx
+++ src/main.tsx
@@ -31,6 +31,7 @@ import './r3-map-hierarchy.css';
 import './r3-terrain-prototype.css';
 import './responsive-command-fit.css';
 import './r3-wp6-command-ui.css';
+import './r3-wp6-pictorial-details.css';
 import { installMapTrackpadGuard } from './map-trackpad-guard';
 import { installR3MapVisualGrading } from './presentation/r3-map-visual-grading';
 
